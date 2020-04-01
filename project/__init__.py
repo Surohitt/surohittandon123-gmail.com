7371b31ab449ad9fa6cf2de9911edb0acc44da20
Surohitt <surohittandon123@gmail.com>
2020-03-31 22:19:11 -0700

    wip

--- project/__init__.py
+++ project/__init__.py
@@ -1,17 +1,37 @@
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # Instantiate App using reserved name variable from python cli
 app = Flask(__name__)
 
 api = Api(app)
 
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# Instantiate db
+db = SQLAlchemy(app)
+
+# Create User model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False) 
+    active = db.Column(db.Boolean, default=True, nullable=False) 
+    
+    def __init__(self):
+        self.username = username
+        self.email = email
 
 class Ping(Resource):
     def get(self):
         return {
-                'status': 'success',
-                'message': 'pong!'
+                'message': 'pong!',
+                'status': 'success'
                 }
 
 
